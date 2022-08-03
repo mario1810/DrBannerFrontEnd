1feb20ad6e7da9d3774e760c33c8e05333173041
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-08-03 02:06:24 -0500

    Add: Post al pasar a la página de pago y/o salir de la página carrito

--- js/carrito.js
+++ js/carrito.js
@@ -2,6 +2,9 @@ const USER_INFO_GET_URL = "/assets/json/carritoPaquetesGet.json";
 const USER_INFO_POST_URL = "https://a75973a6-cd56-4429-816a-7f6527bc347e.mock.pstmn.io";
 const SERVICE_TYPE = "Json"; //Fetch
 const KEY_LS= "userLS";
+const SIMULAR_ENVIO=true;
+let idUser=0; //temporalmente global xD
+
 
 const tablaBody=document.getElementById("tablaBody");
 const mensajeCarritoVacio=document.getElementById("carritoVacio");
@@ -12,13 +15,24 @@ const btnPagar=document.getElementById("btnPagar");
  * Se lamma la función tan pronto se cargue el documento HTML
  */
  window.addEventListener('load', ()=> {solicitudPaquetesCarrito()});
-
+ /**
+  * Función que se llama cuando se abanadona la página
+  */
+ window.addEventListener("beforeunload",()=> {guardarCarrito()});
  /**
   * Cuando se da click en el botón de pagar
   */
- btnPagar.addEventListener("click",()=>{window.location.assign("/html/pago.html")});
- 
-/**
+ btnPagar.addEventListener("click",()=>{EnvioInfoAPI(true,true)});
+
+
+
+
+ /**
+  * 
+  *                                 GET
+  */
+
+ /**
  * 
  * Función que realiza el fetch para obtener los paquetes que el usuario agrego al carrito
  * 
@@ -126,9 +140,92 @@ async function solicitudPaquetesCarrito() {
     }
 }
 
+/**
+ * 
+ *                                           POST    
+ */
+
+
+ /**
+  *     FUnción que realiza un post a la api
+  * @param {*} direccionhttp  // dirección de la API
+  * @param {*} json  // JSON que se adjunta en el post
+  */
+  async function enviarDatos(direccionhttp, data){
+    return new Promise((resolve, reject) => {
+      fetch(direccionhttp,{
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
+        }
+
+      }).then((responseJSON) =>{responseJSON.json()})
+        .catch((error) => {
+          console.log(error);
+          reject(false);
+        })
+        .then((response) => {
+            console.log('Success:', response);
+            resolve(true);
+          })
+    });
+}
 
 
 
+/**
+ * Crea el objeto que será enviado  a través del post
+ * compra=Indica si se está realizadon la compra, o sólo se almacena el carrito 
+ */
+function crearObjetoPost(compra){
+    //Creamos un objeto con la información a enviar
+    let totalC=String(totalPagar.innerHTML);
+    let localData=localStorage.getItem(KEY_LS);
+    //Obtenemos los datos como un objeto
+    let localDataObject=JSON.parse(localData);
+    //Obtenemos el arreglo de paquetes
+    let arrayCarrito=localDataObject.carrito;
+    let user = {
+        id: Number(idUser),
+        total: Number(totalC.substring(1,totalC.length)),
+        realizoCompra:String(compra),
+        carrito: arrayCarrito,
+    };
+    return user;
+}
+/**
+ * 
+ * Función que creará el objeto con la informacióna a enviar a la API
+ * 
+ */
+async function EnvioInfoAPI(showMensaje, compra){
+    
+    let flag=true;
+    let user=crearObjetoPost(compra);
+    if(!SIMULAR_ENVIO){
+        flag = await enviarDatos(USER_INFO_POST_URL,user);
+    }
+    if(flag){ // Envio a ala API exitoso
+        localStorage.clear();
+        window.location.assign("/html/pago.html");
+    }else{
+        //Ocurrio un error
+        if(showMensaje){
+            alert("Ocurrio un error, volver a intentarlo");
+        }
+    }
+}
+
+
+function guardarCarrito(){
+    let localData=localStorage.getItem(KEY_LS);
+    //Significa que el usuario no realizó la compra y se dirigió a otra página (Porque al realizar la compra, borramos la memoria local)
+    //Y por tanto debemos guardar el carrito en la API
+    if(localData!=null){
+        EnvioInfoAPI("false","false");
+    }
+}
 
 
 
