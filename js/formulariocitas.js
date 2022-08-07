70ea46e7807ea92bc2030622c32c44549444fc30
marcantonio13 <107515956+marcantonio13@users.noreply.github.com>
2022-08-07 16:50:27 -0500

    texto de entrada declaracion

--- js/formulariocitas.js
+++ js/formulariocitas.js
@@ -1,3 +1,5 @@
+// Declaracion decontantes 
+
 const inputNombre=document.getElementById("nombre");
 const inputApellido=document.getElementById("apellido");
 const inputCorreo=document.getElementById("correo");
@@ -6,4 +8,17 @@ const inputFecha=document.getElementById("fecha");
 const selectPaquete=document.getElementById("paquete");
 const selectTipoPaquete=document.getElementById("tipoPaquete");
 const textaraDireccion=document.getElementById("direccion");
+
+
+//eventos de ingreso de texto (despues de que el valor ha llegado al input)
+inputNombre.addEventListener("keyup",(event)=>{corroborarNombre()});
+inputTarjeta.addEventListener("keyup",(event)=>{corroborarApellido()});
+inputMes.addEventListener("keyup",(event)=>{corroborarCorreo()});
+inputYear.addEventListener("keyup",(event)=>{corroborarTelefono(event)});
+inputCVV.addEventListener("keyup",(event)=>{corroborarFecha()});
+inputCVV.addEventListener("keyup",(event)=>{corroborarPaquete()});
+inputCVV.addEventListener("keyup",(event)=>{corroborartipoPaquete()});
+inputCVV.addEventListener("keyup",(event)=>{corroborarDireccion()});
+
+
 
