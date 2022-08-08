0470d92470a556469100e235809aad0b9a205f7d
emperatrizm <emperatriz.668@gmail.com>
2022-08-07 23:34:17 -0500

    adding redirect to login in Agenda tu sesion  button

--- js/main.js
+++ js/main.js
@@ -29,6 +29,26 @@ const isUserLogged = () => {
     return Boolean(getCookie('session_id'));
 };
 
+const bindGotoLoggedListener = () => {
+    const buttons = document.querySelectorAll('[data-loggedhref]');// buscar todos los botones con el data atributo loggedhref 
+    for (button of buttons) {
+        //Por cada boton encontrado agrega un listener al evento click
+        button.addEventListener('click', () => {
+            //Cuando el boton es clicked
+            if(isUserLogged() === true) { // si el usuario esta logueado 
+                //ir a la pagina que desea.
+                window.location.assign(button.dataset.loggedhref);
+            } else {
+                // si no, guardar la cookie la pagina a donde debe ir despues del login.
+                setCookie('post_login_redirect',  window.location.origin + button.dataset.loggedhref); 
+                window.location.assign("/html/login.html");
+            }
+            
+        });
+    }
+};
 
-
+window.addEventListener('DOMContentLoaded', () => {
+    bindGotoLoggedListener();
+});
 
