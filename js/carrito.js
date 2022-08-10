c013f7a4a030bb33bad144b8bf0032d8c6a72862
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-08-10 00:36:09 -0500

    Add: Se añade userID y isUserLogged a página de carrito

--- js/carrito.js
+++ js/carrito.js
@@ -2,20 +2,22 @@ const USER_INFO_GET_URL = "/assets/json/carritoPaquetesGet.json";
 const USER_INFO_PUT_URL = "";
 const USER_INFO_DELETE_URL = "";
 const SERVICE_TYPE = "Json"; //Fetch
+const MENSAJE_CAR_VACIO="Vive tu momento,<br>nosotros lo capturamos<br><br>¿Ya has visto nuestros paquetes?<br><br>";
 const SIMULAR_ENVIO=true;
 let arrayCarrito;
-let idUser=0; //temporalmente global xD
+//let idUser=0; //temporalmente global 
 
 
 const tablaBody=document.getElementById("tablaBody");
 const mensajeCarritoVacio=document.getElementById("carritoVacio");
 const totalPagar=document.getElementById("totalPagar");
 const btnPagar=document.getElementById("btnPagar");
+const mensajeSub= document.getElementById("numPaquetesTitulo");
 
 /**
  * Se lamma la función tan pronto se cargue el documento HTML
  */
- window.addEventListener('load', ()=> {obtenerPaquetes()});
+ window.addEventListener('DOMContentLoaded', ()=> {obtenerPaquetesUsuario()});
  /**
   * Función que se llama cuando se abanadona la página
   */
@@ -24,7 +26,7 @@ const btnPagar=document.getElementById("btnPagar");
   * Cuando se da click en el botón de pagar
   */
  btnPagar.addEventListener("click",()=>{EnvioInfoAPI()});
-
+ 
 
 
 
@@ -75,6 +77,7 @@ async function requestGet(proveedor = "Fetch", direccionhttp) {
  */
 function muestraPaquetesTabla(arrayPaquetes){
     let pagar=0;
+    mensajeSub.innerHTML=(arrayPaquetes.length==1)?"1 paquete":(arrayPaquetes.length+" paquetes");
     mensajeCarritoVacio.innerHTML="";
     tablaBody.innerHTML="";
     totalPagar.innerHTML="$"+Number.parseFloat(0).toFixed(2);
@@ -98,29 +101,35 @@ function muestraPaquetesTabla(arrayPaquetes){
         }
         totalPagar.innerHTML="$"+Number.parseFloat(pagar).toFixed(2);
     }else{
-        btnPagar.disabled=true;
-        mensajeCarritoVacio.innerHTML=`Tu carrito de compras está vacio`;
+        btnPagar.disabled=true;        
+        mensajeCarritoVacio.innerHTML=MENSAJE_CAR_VACIO;
     }
 }
 
 /**
  * Función que obtiene los paquetes que se han añadido en el carrito
  */
- async function obtenerPaquetes() {
-    
+async function obtenerPaquetesUsuario() {
+
+  let userId = getUserId();
+  if (userId != null && isUserLogged() === true) {
     //Realizamos un fetch
-    let usuario = await requestGet(SERVICE_TYPE,USER_INFO_GET_URL);
+    let usuario = await requestGet(SERVICE_TYPE, USER_INFO_GET_URL);
     // El fetch se realizó de manera correcta?
-    if(usuario!=null){ 
-        //Guardamos al información del carrito localmente
-        arrayCarrito=usuario.carrito;
-        //Mostramos la info en la tabla
-        muestraPaquetesTabla(arrayCarrito);
-    }else{
-        //Condición de protección
-        //alert("Recargar la página");
-        mensajeCarritoVacio.innerHTML=`Tu carrito de compras está vacio`;
+    if (usuario != null) {
+      //Guardamos al información del carrito localmente
+      arrayCarrito = usuario.carrito;
+      //Mostramos la info en la tabla
+      muestraPaquetesTabla(arrayCarrito);
+      return;
     }
+  }
+  //Condición de protección
+  //alert("Recargar la página");
+  btnPagar.disabled=true; 
+  totalPagar.innerHTML="$"+Number.parseFloat(0).toFixed(2);
+  mensajeSub.innerHTML = "0 paquetes";
+  mensajeCarritoVacio.innerHTML = MENSAJE_CAR_VACIO;
 }
 
 /**
@@ -131,18 +140,22 @@ function muestraPaquetesTabla(arrayPaquetes){
 /**
  * Función que crea evento  listener para cada bote de basura
  */
-function borrarRow(id, paqueteId){
-    if(arrayCarrito.length>0){
-        if(!SIMULAR_ENVIO){
-          let data =String(idUser+"&"+paqueteId);
-          requestDelete(USER_INFO_DELETE_URL,data);
-        } 
-        arrayCarrito.splice(id,1);
-        muestraPaquetesTabla(arrayCarrito);
-    }else{
-        //Condición de protección
-        mensajeCarritoVacio.innerHTML=`Tu carrito de compras está vacio`;
+function borrarRow(id, paqueteId) {
+  let userId=getUserId();
+  if(userId==null || isUserLogged() === false){
+    window.location.reload(); //recargamos la página porque ya vencio la sesión
+  }
+  if (arrayCarrito.length > 0) {
+    if (!SIMULAR_ENVIO) {
+      let data = String(userId + "&" + paqueteId);
+      requestDelete(USER_INFO_DELETE_URL, data);
     }
+    arrayCarrito.splice(id, 1);
+    muestraPaquetesTabla(arrayCarrito);
+  } else {
+    //Condición de protección
+    mensajeCarritoVacio.innerHTML = MENSAJE_CAR_VACIO;
+  }
 }
 
 /**
@@ -209,25 +222,29 @@ function borrarRow(id, paqueteId){
  * Función que creará el objeto con la informacióna a enviar a la API
  * 
  */
-async function EnvioInfoAPI(){
-    let flag=true;
-     //Creamos un objeto con la información a enviar
-     let totalC=String(totalPagar.innerHTML);
-     let user = {
-         id: Number(idUser),
-         total: Number(totalC.substring(1,totalC.length)),
-     };
+async function EnvioInfoAPI() {
+  let flag = true;
+  //Creamos un objeto con la información a enviar
+  let totalC = String(totalPagar.innerHTML); // Para quitar el simbolo $
 
-    if(!SIMULAR_ENVIO){
-        flag = await requestPut(USER_INFO_PUT_URL,user);
-    }
-    if(flag){ // Envio a ala API exitoso
-        window.location.assign("/html/pago.html");
-    }else{
-        alert("Ocurrio un error, recargar la página");        
-    }
+  let userId = getUserId();
+  if (userId == null || isUserLogged() === false)
+      window.location.reload(); //recargamos la página porque ya vencio la sesión
+  
+  //Creamos el objeto a enviar como json
+  let user = {
+    id: Number(userId),
+    total: Number(totalC.substring(1, totalC.length)),
+  };
+
+  if (!SIMULAR_ENVIO) {
+    flag = await requestPut(USER_INFO_PUT_URL, user);
+  }
+  if (flag) { // Envio a la API exitoso
+    window.location.assign("/html/pago.html");
+  } else {
+    alert("Ocurrio un error, recargar la página");
+  }
 }
 
 
-
-  
