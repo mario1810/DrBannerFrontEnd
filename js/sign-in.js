8a94cc7205352efcfa4066c0022c1de1b0e07b85
Gerardo Ruiz <ruiz.gerardo.r@gmail.com>
2022-08-12 12:49:34 -0500

    ultimos cambios

--- js/sign-in.js
+++ js/sign-in.js
@@ -217,3 +217,21 @@ function mostrarContraseña() {
     visible.type = "password";
   }
 }
+
+const formSignin = document.getElementById('form-signin'); 
+ 
+  formSignin.addEventListener('submit', (event) => { 
+      event.preventDefault(); 
+      //enviar request para  iniciar sesion en el backend; 
+   
+      const sessionId = Date.now();// solo para probar, esto debe venir del backend 
+       //code - success  
+      setCookie('session_id', sessionId, 3); // cookie name, valor, numero de dias para expirar 
+      let redirectTo = document.referrer;// de donde venias caso 1 
+      if (getCookie('post_login_redirect') ) { //caso2 
+          redirectTo = getCookie('post_login_redirect'); 
+          deleteCookie('post_login_redirect'); 
+      } 
+      window.location.href = redirectTo; 
+      //code - success 
+  })
