b09c2eb7127ea8bb8afc164f2897a8d98f871f2c
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-09-04 23:49:46 -0500

    Ajustes JS perfil para integrar backend, info usuario, tarjeta compras

--- js/perfil.js
+++ js/perfil.js
@@ -1,59 +1,64 @@
-const URL_COMENT_LOCAL="/assets/json/perfil.json"; //Provicional en lo que se genera el de login 
-const URL_COMENT_LOCAL2="/assets/json/perfil.pago.json";
-const URL_COMENT_LOCAL3="/assets/json/carritoPaquetesGet.json";
-//const URL_COMENT_LOCAL="/assets/json/perfilprueba.json";
-//const URL_COMENT_LOCAL2="/assets/json/perfilpagoprueba.json";
-//const URL_COMENT_LOCAL3="/assets/json/perfilpaquetesprueba.json";
+const URL_COMENT_LOCAL="http://localhost:8080/api/usuario";
+const URL_COMENT_LOCAL2="http://localhost:8080/api/usuario/tarjeta";
+const URL_COMENT_LOCAL3="http://localhost:8080/api/usuario/historial";
+//const URL_COMENT_LOCAL="/assets/json/perfil.json";
+//const URL_COMENT_LOCAL2="/assets/json/perfil.pago.json";
+//const URL_COMENT_LOCAL3="/assets/json/carritoPaquetesGet.json";
+
 const saludoM= document.getElementById("saludo");
 
 function cargarDatosPersonales(){
-    fetch(URL_COMENT_LOCAL) //el nombre del archivo JSON donde se extraeran los datos
+    fetch((URL_COMENT_LOCAL+"/"+String(localStorage.getItem("userId")))) //el nombre del archivo JSON donde se extraeran los datos
     .then(respuesta=> respuesta.json()) //indica formato en el que se desea obtener la info
     .then(datos=> {               //datos=respuesta.json (array)
         console.log("datos personales");
-        console.log(datos.data); 
-        return mostrarDatos(datos.data);
+        console.log(datos); 
+        return mostrarDatos(datos);
     })
            
     }
     //data-title="Nombre" ,  data-title="Apellidos" ,  data-title="Correo" ,  data-title="Contraseña" ,  data-title="Telefono"
 
 
-    function mostrarDatos(arreglo) {
-        let datos = "";
-        for (let usuario of arreglo) {
-            datos = datos + `<tr>
+    function mostrarDatos(usuario) {
+        saludoM.innerHTML="Bienvenid@ "+usuario.nombre;
+        let datos = `<tr>
             <td data-title="Nombre">${usuario.nombre}</td>  
             <td data-title="Apellidos">${usuario.apellido}</td>
             <td data-title="Correo">${usuario.correo}</td>   
-            <td data-title="Contraseña">${usuario.contra}</td>
+            <td data-title="Contraseña">${usuario.password}</td>
             <td data-title="Telefono"> ${usuario.telefono}</td>
             </tr>`;
-        }
         document.getElementById("datosPersonales").innerHTML = datos;
-        saludoM.innerHTML="Bienvenid@ "+arreglo[0].nombre;
     }
         
 
 function cargarFormaDePago(){
-    fetch(URL_COMENT_LOCAL2) //el nombre del archivo JSON donde se extraeran los datos
+    fetch((URL_COMENT_LOCAL2+"/"+String(localStorage.getItem("userId")))) //el nombre del archivo JSON donde se extraeran los datos
     .then(res=> res.json()) //indica formato en el que se desea obtener la info
     .then(datos=> {               //datos=respuesta.json (array)
        console.log("datos usuario");
-        console.log(datos.usuario); 
-        return mostrarDatosPago(datos.usuario);
+        console.log(datos); 
+        return mostrarDatosPago(datos);
     })
            
     }
     
-    function mostrarDatosPago(arregloPago) {
-        let datosPago = "";
-        for (let pago of arregloPago) {
-            datosPago = datosPago + `<tr>
+    function mostrarDatosPago(pago) {
+        let datosPago ="";
+        
+        if(pago.datosTarjeta){
+            datosPago = `<tr>
             <td data-title="Nombre">${pago.nombre}</td>
             <td data-title="Apellido">${pago.apellido}</td>
-            <td data-title="Tarjeta">${pago.tarjeta.tipo}</td>                   
-            <td data-title="Numero">${pago.tarjeta.numeroTarjeta}</td>
+            <td data-title="Tarjeta">${pago.tipoTarjeta}</td>                   
+            <td data-title="Numero">${pago.numeroTarjeta}</td>
+            </tr>`; //Linea 46 deberia ser tarjeta.pago , pero la wabada bota error(grita en silencio)
+        }else{
+            `<tr>
+            <td data-title="Nombre">${pago.nombre}</td>
+            <td data-title="Apellido">${pago.apellido}</td>
+            <td colspan = "2" >No hay datos de tarjeta registrados</td>                   
             </tr>`; //Linea 46 deberia ser tarjeta.pago , pero la wabada bota error(grita en silencio)
         }
         document.getElementById("datosFormaPago").innerHTML = datosPago;
@@ -63,34 +68,61 @@ function cargarFormaDePago(){
      
    
 function cargarHistorialCompra(){
-        fetch(URL_COMENT_LOCAL3) //el nombre del archivo JSON donde se extraeran los datos
+        fetch((URL_COMENT_LOCAL3+"/"+String(localStorage.getItem("userId")))) //el nombre del archivo JSON donde se extraeran los datos
         .then(respuestaHistorial=> respuestaHistorial.json()) //indica formato en el que se desea obtener la info
         .then(datos=> {       
-            console.log(datos.usuario);        //datos=respuesta.json (array)
+            console.log(datos);        //datos=respuesta.json (array)
             // console.log("compras");
             // let array=datos.usuario;
             // console.log(array); 
             // console.log(typeof(array));
-            let usuarioArray= datos.usuario;
-            console.log(usuarioArray.carrito);
-            mostrarDatosCompra(usuarioArray.carrito);
+            
+            mostrarDatosCompra(datos);
             //return mostrarDatosCompra(datos.usuario);
         })
             
                
         }
         
-    function mostrarDatosCompra(arregloPaquete) {
+    function mostrarDatosCompra(arregloCompras) {
             let datosAnteriores = "";
-            for (let paquete of arregloPaquete) {
-                //id paquete tipo costo fecha
+            let num=1;
+            for (let compra of arregloCompras) {
+                let paquetesStr="";
+                for(let paquete of compra.pedidos){
+                    paquetesStr+=`<tr>
+                    <td>${paquete.fecha}</td> 
+                    <td>${paquete.direccion}</td>
+                    <td>${paquete.nombrePaquete}</td>   
+                    <td>${paquete.nombreCategoria}</td>   
+                    <td>${paquete.costo}</td>   
+                    </tr>`
+                }
                 datosAnteriores = datosAnteriores + `<tr>
-                <td>${paquete.id}</td> 
-                <td>${paquete.paquete}</td>
-                <td>${paquete.tipo}</td>   
-                <td>${paquete.costo}</td>
-                <td>${paquete.fecha}</td>
-                </tr>`;
+                <td colspan = "1"><b>#Compra</b></td> 
+                <td colspan = "1"><b>Fecha Compra</b></td> 
+                <td><b>Hora Compra</b></td>
+                <td colspan = "2"><b>CostoToral</b></td>   
+                </tr>
+                
+                <tr>
+                <td colspan = "1">${num}</td> 
+                <td colspan = "1">${compra.fechaCompra}</td> 
+                <td>${compra.horaCompra}</td>
+                <td colspan = "2">${compra.costoTotal}</td>   
+                </tr>
+                
+                <tr>
+                    <td><b>fecha</b></td> 
+                    <td><b>direccion</b></td>
+                    <td><b>Paquete</b></td>   
+                    <td><b>Categoria</b></td>   
+                    <td><b>costo</b></td>   
+                    </tr>`
+                
+                +paquetesStr;
+                ;
+                num++;
             }
             document.getElementById("datosHistorialCompras").innerHTML = datosAnteriores;
         }
